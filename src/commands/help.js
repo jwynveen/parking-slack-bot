06042ee43a9b484c321bfe92844a5c3bdb1dfec4
Jon Wynveen <jwynveen@mpirik.com>
2016-06-07 20:30:29 -0500

    Initialize from starbot

---
+++ src/commands/help.js
@@ -0,0 +1,39 @@
+
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Parkbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+let attachments = [
+  {
+    title: 'Parkbot will help you find the available parking spots',
+    color: '#2FA44F',
+    text: '`/park available` returns available parking spots',
+    mrkdwn_in: ['text']
+  },
+  {
+    title: 'Configuring Parkbot',
+    color: '#E3E4E6',
+    text: '`/star help` ... you\'re lookin at it! \n',
+    mrkdwn_in: ['text']
+  }
+]
+
+const handler = (payload, res) => {
+  let msg = _.defaults({
+    channel: payload.channel_name,
+    attachments: attachments
+  }, msgDefaults)
+
+  res.set('content-type', 'application/json')
+  res.status(200).json(msg)
+  return
+}
+
+module.exports = { pattern: /help/ig, handler: handler }
